39558d716d13afe8805fe26d712a44a69bcc0669
cjd0302 <cjd0302@gmail.com>
2022-11-28 20:24:03 +0900

    no message

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -2,19 +2,17 @@
   <div>
     <section>
 		<!-- 질문 상세 정보-->
-		<div class="user-container">
-			<div>
-				<i class="fas fa-user"></i>
-			</div>
-			<div class="user-description">
-				<router-link :to="`/user/${fetchedItem.user}`">
-					{{ fetchedItem.user}}
-				</router-link>
-				<div class="time">
-					{{fetchedItem.time_ago}}
-				</div>
-			</div>
-		</div>
+		
+		<user-profile :info="fetchedItem">
+			<!-- <div slot="username">{{fetchedItem.user}}</div> -->
+			<router-link slot="username" :to="`/user/${fetchedItem.user}`">
+				{{ fetchedItem.user }}
+			</router-link>
+			<div slot="time">{{'Posted ' + fetchedItem.time_ago}}</div>
+		</user-profile>
+		
+	</section>
+	<section>
 		<h2>{{ fetchedItem.title }}</h2>
 	</section>
 	<section>
@@ -24,8 +22,12 @@
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
 import {mapGetters} from 'vuex';
 export default {
+	components : {
+		UserProfile
+	},
 	computed :{
 		...mapGetters(['fetchedItem'])
 	},
